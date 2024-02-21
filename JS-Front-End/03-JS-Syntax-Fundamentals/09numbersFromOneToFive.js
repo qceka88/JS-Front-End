c02067132a81ddcf86542d54764e115bf0a7ed8c
Yanko Rashkov <thewhitedead@abv.bg>
2024-02-21 17:48:11 +0200

    Create 09numbersFromOneToFive.js

---
+++ JS-Front-End/03-JS-Syntax-Fundamentals/09numbersFromOneToFive.js
@@ -0,0 +1,32 @@
+function printNumbers() {
+    for (let i=1; i <= 5; i++) {
+        console.log(i)
+    }
+}
+
+printNumbers();
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+
+                            9.\tNumbers from 1 to 5
+Write a function that prints all the numbers from 1 to 5 (inclusive) each on a separate line.
+
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+Null
+
+Output
+1
+2
+3
+4
+5
+
+
+```
