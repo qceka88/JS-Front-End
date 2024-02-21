3a25405a3eb50295d97bc6389f15898df6504876
Yanko Rashkov <thewhitedead@abv.bg>
2024-02-21 16:13:34 +0200

    Create 06largestNumber.js

---
+++ JS-Front-End/03-JS-Syntax-Fundamentals/06largestNumber.js
@@ -0,0 +1,40 @@
+function largestNumber(...numbers) {
+    let result = Math.max(...numbers);
+    console.log(`The largest number is ${result}.`);
+}
+
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
+
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+
+                               6.\tLargest Number
+                               
+Write a function that takes three number arguments as input and finds the largest of them. Print the 
+following text on the console:  \`The largest number is {number}.\`.
+The input comes as three number arguments passed to your function.
+The output should be printed to the console.
+
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+5, -3, 16
+
+Output
+The largest number is 16.
+____________________________________________________________________________________________
+Example_02
+
+Input
+-3, -5, -22.5
+
+Output
+The largest number is -3.
+
+```
