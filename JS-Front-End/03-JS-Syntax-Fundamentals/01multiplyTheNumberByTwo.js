9f23fb519f7ae5879e3757b78df96758c2e2cd26
Yanko Rashkov <thewhitedead@abv.bg>
2024-02-21 15:15:43 +0200

    Created JS-Syntax-Fundamentals

---
+++ JS-Front-End/03-JS-Syntax-Fundamentals/01multiplyTheNumberByTwo.js
@@ -0,0 +1,43 @@
+function multiplyByTwo(num) {
+    let multiplier = 2;
+    console.log(num * multiplier);
+}
+
+multiplyByTwo(2);
+multiplyByTwo(5);
+multiplyByTwo(20);
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                    1. Multiply the Number by 2
+                    
+Write a function that receives a number and prints as result that number multiplied by two.
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+2
+
+Output
+4
+____________________________________________________________________________________________
+Example_02
+
+Input
+5
+
+Output
+10
+____________________________________________________________________________________________
+Example_03
+
+Input
+20
+
+Output
+40
+
+```
