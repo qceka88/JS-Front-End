fb5a3427acab920c01486043012c7cddcb8121b4
Yanko Rashkov <thewhitedead@abv.bg>
2024-02-21 15:28:28 +0200

    Create 03excellentGrade.js

---
+++ JS-Front-End/03-JS-Syntax-Fundamentals/03excellentGrade.js
@@ -0,0 +1,41 @@
+function checkGrade(grade) {
+    if (grade >= 5.50) {
+        console.log('Excellent');
+    } else {
+        console.log('Not excellent');
+    }
+}
+
+
+checkGrade(5.50);
+checkGrade(4.35);
+
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+
+                                3. Excellent Grade
+Write a function that receives a single number and checks if the grade is excellent or not. 
+If it is, print "Excellent", otherwise print "Not excellent".
+
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+5.50
+
+Output
+Excellent
+____________________________________________________________________________________________
+Example_02
+
+Input
+4.35
+
+Output
+Not excellent
+
+```
