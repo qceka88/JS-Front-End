f3691398547e212e224bf84c4de9ee6f850f3451
Yanko Rashkov <thewhitedead@abv.bg>
2024-02-24 17:06:01 +0200

    Create 07charsToString.js

---
+++ JS-Front-End/04-JS-Syntax-Fundamentals-Exercise/07charsToString.js
@@ -0,0 +1,40 @@
+function charsToString(firstElement, secondElement, thirdElement) {
+    let output = `${firstElement}${secondElement}${thirdElement}`;
+
+    console.log(output);
+}
+
+charsToString('a', 'b', 'c');
+charsToString('%', '2', 'o');
+charsToString('1', '5', 'p');
+
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                                      7.\tChars to String
+Write a function, which receives 3 parameters. Each parameter is a single character. Combine all the characters into one string and print it on the console.
+
+
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+'a', 'b', 'c'
+
+Output
+abc
+
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+'%', '2', 'o'
+
+Output
+%2o
+
+```
