0b35521490f62fff3ea71c4aa115a0f45c64202e
Yanko Rashkov <thewhitedead@abv.bg>
2024-02-24 17:09:12 +0200

    Create 08reversedChars.js

---
+++ JS-Front-End/04-JS-Syntax-Fundamentals-Exercise/08reversedChars.js
@@ -0,0 +1,37 @@
+function reversedChars(firstElement, secondElement, thirdElement) {
+    let output = `${thirdElement} ${secondElement} ${firstElement}`;
+
+    console.log(output);
+}
+
+reversedChars('A','B','C');
+reversedChars('1','L','&');
+
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                                      8.\tReversed Chars
+Write a program that takes 3 parameters (characters) and prints them in reversed order with a space between them.
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+'A', 'B', 'C'
+
+Output
+C B A
+
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+'1', 'L', '&'
+
+Output
+& L 1
+
+```
