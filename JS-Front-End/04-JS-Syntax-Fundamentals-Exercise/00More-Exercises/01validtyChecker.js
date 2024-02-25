754548a48f0c7dc3d3c11e9f583289d6309b28e8
Yanko Rashkov <thewhitedead@abv.bg>
2024-02-25 15:03:20 +0200

    Update 01validtyChecker.js

--- JS-Front-End/04-JS-Syntax-Fundamentals-Exercise/00More-Exercises/01validtyChecker.js
+++ JS-Front-End/04-JS-Syntax-Fundamentals-Exercise/00More-Exercises/01validtyChecker.js
@@ -26,3 +26,35 @@ function validityChecker(x01, y01, x02, y02) {
 
 validityChecker(3, 0, 0, 4);
 validityChecker(2, 1, 1, 1);
+
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                        JS Syntax Fundamentals – More Exercises
+Problems with exercise and homework for the "JS Front-End" Course @ SoftUni. 
+1.\t*Validity Checker
+Write a program that receives a total of 4 parameters in the format x1, y1, x2, y2. Check if the distance between each point (x, y) and the beginning of the Cartesian coordinate system (0, 0) is valid. A distance between two points is considered valid if it is an integer value. 
+Note: You can use the following formula to help you calculate the distance between the points (x1, y1) and (x2, y2).
+The order of comparisons should always be first {x1, y1} to {0, 0}, then {x2, y2} to {0, 0} and finally {x1, y1} to {x2, y2}. 
+In case a distance is valid, print: \`{x1, y1} to {x2, y2} is valid\`
+If the distance is invalid, print: \`{x1, y1} to {x2, y2} is invalid\`
+The input consists of two points given as 4 numbers.
+For each comparison print either \`{x1, y1} to {x2, y2} is valid\` if the distance is valid, or \`{x1, y1} to {x2, y2} is invalid\` if it is invalid.
+
+
+
+
+____________________________________________________________________________________________
+Example
+
+Input
+2, 1, 1, 1
+
+Output
+{2, 1} to {0, 0} is invalid
+{1, 1} to {0, 0} is invalid
+{2, 1} to {1, 1} is valid
+
+
+```
