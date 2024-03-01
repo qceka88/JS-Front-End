19465d47c759c6e9f8531a2d21676a8b1bfc47c7
Yanko Rashkov <thewhitedead@abv.bg>
2024-03-01 22:44:46 +0200

    Create 04substring.js

---
+++ JS-Front-End/05-JS-Arrays-&-Strings-Lab/04substring.js
@@ -0,0 +1,31 @@
+function substringCheck(someString, start, quantity) {
+    let newString = someString.slice(start, start + quantity);
+
+    console.log(newString);
+}
+
+
+substringCheck('ASentence', 1, 8);
+substringCheck('SkipWord', 4, 7);
+
+
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                                 4.\tSubstring
+Write a function that receives a string and two numbers. The numbers will be a starting 
+index and count of elements to substring. Print the result.
+
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+[1,2,3,4,5,6]
+
+Output
+3
+
+```
