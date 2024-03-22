445496dd507e58cf41f270ba02f4e3e77decefc9
Yanko Rashkov <thewhitedead@abv.bg>
2024-03-22 22:12:34 +0200

    create 04-Show-More

---
+++ JS-Front-End/11-DOM-Introduction/04-Show-More/ShowMore.js
@@ -0,0 +1,19 @@
+function showText() {
+    let readMore = document.getElementById('more');
+    let text = document.getElementById('text');
+
+    readMore.style.display = 'none';
+    text.style.display = 'inline';
+}
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                              4.\tShow More
+Write a JS function that expands a hidden section of text when a link is clicked. 
+The link should disappear as the rest of the text shows up.
+Input/Output
+There will be no input/output, your program should instead modify the DOM of the given HTML document.
+
+
+```
