33fe61924190b9e7d962029fd8efb6a373cdaf32
Yanko Rashkov <thewhitedead@abv.bg>
2024-03-23 18:34:42 +0200

    Update 01classStorage.js

--- JS-Front-End/10-Objects-and-Classes-Exercise/00More-Exercises/01classStorage.js
+++ JS-Front-End/10-Objects-and-Classes-Exercise/00More-Exercises/01classStorage.js
@@ -28,3 +28,51 @@ storage.addProduct(productThree);
 console.log(storage.getProducts());
 console.log(storage.capacity);
 console.log(storage.totalCost);
+
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                               1.\tClass Storage
+Create a class Storage. It should have the following properties, while the constructor should only receive a capacity:
+•\tcapacity – a number that decreases when adding a given quantity of products to storage
+•\tstorage – list of products (object). Each product should have:
+o\tname - a string
+o\tprice – a number (price is for a single piece of product)
+o\tquantity – a number
+•\ttotalCost – the sum of the cost of the products
+The class should also have the following methods:
+•\taddProduct – a function that receives a product and adds it to the storage
+•\tgetProcuts – a function that returns all the products in storage in JSON format, each on a new line
+Paste only the class Storage in judge (Note: all names should be as described)
+
+
+
+
+____________________________________________________________________________________________
+Example
+
+Input
+let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
+let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
+let productThree = {name: 'Bread', price: 1.10, quantity: 8};
+let storage = new Storage(50);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
+console.log(storage.getProducts());
+console.log(storage.capacity);
+console.log(storage.totalCost);
+
+
+
+Output
+{"name":"Cucamber","price":1.5,"quantity":15}
+{"name":"Tomato","price":0.9,"quantity":25}
+{"name":"Bread","price":1.1,"quantity":8}
+2
+53.8
+
+
+
+```
