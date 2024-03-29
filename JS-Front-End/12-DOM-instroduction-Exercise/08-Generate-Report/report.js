ad6363691d406ee6aa26aae644e42bdddb063de2
Yanko Rashkov <thewhitedead@abv.bg>
2024-03-29 16:53:54 +0200

    Update report.js

--- JS-Front-End/12-DOM-instroduction-Exercise/08-Generate-Report/report.js
+++ JS-Front-End/12-DOM-instroduction-Exercise/08-Generate-Report/report.js
@@ -27,3 +27,28 @@ function generateReport() {
 
     resultElement.value = JSON.stringify(result, null, 2);
 }
+
+//////////////////////////////// TASK CONDITION ////////////////////////////////
+```
+
+                        8.\tGenerate Report
+You will be given a web page, containing a table and output area. 
+ 
+When the "Generate Report" button is pressed:
+•\tYou must generate a JSON report from the data inside the table,
+ by only taking the columns, which are selected.
+Each table header has a checkbox. If the checkbox is checked, then 
+the data from this column must be included in the report. Unchecked columns must be omitted. 
+ 
+For every row (excluding the header):
+•\tCreate an object with properties for each of its columns.
+•\tThe name of each property is the name attribute of the column’s header, 
+and the value is the text content of the cell.
+•\tStore the result in an array and output it as a JSON string 
+display it inside the <textarea> with id "output". See the example for details.
+ 
+Input/Output
+There will be input, your program must execute based on the page content. 
+he output must be a JSON string, displayed in the <textarea> with id "output".
+
+```
