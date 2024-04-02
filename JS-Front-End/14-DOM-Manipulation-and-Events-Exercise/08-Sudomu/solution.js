70ca3091872c141ba05d19bdb355df0f4db933de
Yanko Rashkov <thewhitedead@abv.bg>
2024-04-02 12:45:39 +0300

    08-Sudomu

---
+++ JS-Front-End/14-DOM-Manipulation-and-Events-Exercise/08-Sudomu/solution.js
@@ -0,0 +1,47 @@
+function solve() {
+    const tableElement = document.querySelector('#exercise table');
+    const tBodyElements = document.querySelectorAll('tbody > tr');
+    const checkBtnElement = document.querySelector('#exercise tfoot button:first-of-type');
+    const clearBtnElement = document.querySelector('#exercise tfoot button:last-of-type');
+    const messageElement = document.querySelector('#check p');
+
+    const tBodyAsMatrix = Array.from(tBodyElements)
+        .map(row => Array.from(row.querySelectorAll('td input[type=number]')));
+
+    checkBtnElement.addEventListener('click', (e) => {
+        const matrixLength = tBodyAsMatrix.length
+        let isResolved = true;
+        let [row, col] = [[], []];
+
+        for (let i = 0; i < matrixLength; i++) {
+            const cellX = Number(tBodyAsMatrix[matrixLength - 1][i].value);
+            const cellY = Number(tBodyAsMatrix[i][matrixLength - 1].value);
+
+            if (col.includes(cellY) || row.includes(cellX)) {
+                isResolved = false;
+                break;
+            }
+            col.push(cellY);
+            row.push(cellX);
+
+        }
+
+        if (isResolved) {
+            tableElement.style.border = '2px solid green';
+            messageElement.textContent = 'You solve it! Congratulations!';
+            messageElement.style.color = 'green';
+        } else {
+            tableElement.style.border = '2px solid red';
+            messageElement.textContent = 'NOP! You are not done yet...';
+            messageElement.style.color = 'red';
+        }
+    });
+
+    clearBtnElement.addEventListener('click', (e) => {
+        for (const tRow of tBodyAsMatrix) {
+            tRow.forEach(e => e.value = '');
+        }
+        tableElement.style.border = 'none';
+        messageElement.textContent = '';
+    });
+}
